3d2bbfd6f351d3d06307353e7e968626c34d96bd
Ольга Кушнирова <olgakusnirova@MacBook-Pro-Olga.local>
2022-08-22 22:11:01 +0200

    ajout de request

--- inc/Responce.hpp
+++ inc/Responce.hpp
@@ -6,13 +6,14 @@
 struct responce
 {
 	std::string		_http_version;
-	// std::map<int, std::string>	_status;
 	int				_status_code;
 	std::string		_status_message;
 	std::string		_date;
 	std::string		_server;
 	std::string		_content_type;
 	size_t			_content_length;
+	// std::map<int, std::string>	_status;
+
 };
 
 #endif //WEBSERV_WEBSERV_HPP
