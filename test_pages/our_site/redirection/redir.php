61a53a9d7d0617022010e62b0175622bf0c4c5fa
Anton MYROSHNYCHENKO <amyroshn@e1r1p1.clusters.42paris.fr>
2022-09-16 11:08:13 +0200

    msg small fix

--- test_pages/our_site/redirection/redir.php
+++ test_pages/our_site/redirection/redir.php
@@ -1,6 +1,6 @@
 <?php
 include('../dependencies/header.html');
 echo "<br><br> <h2>Hello, it's redirection test page </h2>";
-echo "<br> <p> Followed link will try to connect to not existing page, but it will be redirected to main page </p>";
+echo "<br> <p> Followed link will try to connect to not existing page, but it will be redirected to autoindex_ok page </p>";
 echo "<a href=\"/not_exist\" target=\"_blank\"> not existing page </a>";
 include('../dependencies/footer.html');
